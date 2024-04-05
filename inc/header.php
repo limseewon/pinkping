bda683a83ac88260462c3dee78ef2ba97b94578e
dayoung <leedayeonggg@gmail.com>
2024-04-05 10:39:03 +0900

    add

---
+++ inc/header.php
@@ -0,0 +1,15 @@
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'].'/pinkping/inc/dbcon.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>dashboard - pinkping</title>
+</head>
+<body>
+  <h1>admin page</h1>
+</body>
+</html>
