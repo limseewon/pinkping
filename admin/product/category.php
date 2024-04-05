5a9c4de9484fb9d06ce1d6376c9af24ece8562f1
dayoung <leedayeonggg@gmail.com>
2024-04-05 13:18:18 +0900

    category

--- admin/product/category.php
+++ admin/product/category.php
@@ -1,53 +1,132 @@
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'] . '/pinkping/inc/header.php';
-$sql = "SELECT * from category where step=1";
-$result = $mysqli -> query($sql);
-while($row = $result->fetch_object()){
-  $cate1[]=$row;
-  // 자바스크립트 버전 = $cate1.push($row);
-  //foreach(대상as별칭){별칭}
+$sql = "SELECT * FROM category where step = 1";
+$result = $mysqli->query($sql);
+while ($row = $result->fetch_object()) {
+  $cate1[] = $row;
 }
-print_r( $cate1);
+print_r($cate1);
 ?>
 
+
 <div class="container">
-  <form action ="">
-      <div class="category row">
-          <div class="col-md-4">
-            <select class="form-select" aria-label="대분류" id="cate1">
-              <option selected>대분류</option>
-                <?php
-                foreach($cate1 as $c1){
-                ?>
-                <option value="<?= $c1->code; ?>"><?= $c1->name; ?></option>
-                <?php  
-                }
-                ?>
-            </select>
-          </div>
-          <div class="col-md-4">
-            <select class="form-select" aria-label="중분류" id="cate2">
-              <option selected disabled>중분류</option>
-              <!-- <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option> -->
-            </select>
-          </div>
-          <div class="col-md-4">
-            <select class="form-select" aria-label="소분류" id="cate3">
-              <option selected>소분류</option>
-              <!-- <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option> -->
-            </select>
-          </div>
+  <form action="">
+    <div class="category row">
+
+
+      <div class="col-md-4">
+
+
+        <select class="form-select" aria-label="대분류" id="cate1">
+          <option selected>대분류</option>
+          <?php
+          foreach ($cate1 as $c1) {
+          ?>
+
+
+            <option value="<?= $c1->code; ?>"><?= $c1->name; ?></option>
+
+
+          <?php
+          }
+          ?>
+
+
+        </select>
       </div>
+      <div class="col-md-4">
+
+
+        <select class="form-select" aria-label="중분류" id="cate2">
+
+
+        </select>
+      </div>
+      <div class="col-md-4">
+
+
+        <select class="form-select" aria-label="소분류" id="cate3">
+
+
+        </select>
+      </div>
+
+
+    </div>
   </form>
 </div>
 <script>
-  $('#cate1').change(function(){
+  $('#cate1').change(function() {
+    makeOption($(this), 2, '중분류', $('#cate2'));
+  });
+  $('#cate2').change(function() {
+    makeOption($(this), 3, '소분류', $('#cate3'));
+  });
+  $('#cate3').change(function() {
 
-  })
+
+  });
+
+
+  // async function makeOption(e, step, category, target) {
+  //   let cate = e.val();
+  //   let data = {
+  //     cate: cate,
+  //     step: step,
+  //     category: category
+  //   };
+  //   console.log(data);
+
+
+  //   try {
+  //     const response = await fetch('printOption.php', {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json'
+  //       },
+  //       body: JSON.stringify(data)
+  //     }
+  //     );
+
+
+  //     if (!response.ok) {
+  //       throw new Error('Network response was not ok');
+  //     }
+
+
+  //     const result = await response.text();
+  //     console.log(result);
+  //     target.innerHTML = result;
+  //   } catch (error) {
+  //     console.error('Error:', error);
+  //   }
+  // }
+
+
+  
+    function makeOption(e, step, category, target) {
+      let cate = e.val();
+      //console.log(cate);
+      // 비동기 방식으로 printOption 값 3개(cate, step, category) 일시키고, 결과가 나오면 target에 html 태그를 생성
+      let data = {
+        cate: cate,
+        step: step,
+        category: category
+      }
+      console.log(data);
+      $.ajax({
+        async: false, // sucess의 결과가 나오면 작업 수행
+        type: 'post',
+        data: data,
+        url: 'printOption.php',
+        dataType: 'html',
+        success: function(result) {
+          console.log(result);
+          target.html(result);
+        }
+      })
+    }
+    
 </script>
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'] . '/pinkping/inc/footer.php';
--- admin/product/printOption.php
+++ admin/product/printOption.php
@@ -6,12 +6,12 @@ $step = $_POST['step'];
 $category = $_POST['category'];
 
 $html = "<option selected disabled>".$category."</option>";
-$sql = "SELECT * FROM category where step = {$step} and pcode = '{#cate}'";
+$sql = "SELECT * FROM category where step = {$step} and pcode = '{$cate}'";
 $result = $mysqli -> query($sql);
 
 while($row = $result -> fetch_object()){
-  // $html .="<option value=\"".$row->code."\">".$row->name."</option>";
-  $html .="<option value=\"$row->code \"> $row->name </option>";
+  $html .="<option value=\"".$row->code."\">".$row->name."</option>";
+  // $html .="<option value=\" $row->code \"> $row->name </option>";
 }
 echo $html;
 ?>
