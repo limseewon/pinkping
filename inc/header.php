b175661fc73045537b844d11b8173d80e3fade33
dayoung <leedayeonggg@gmail.com>
2024-04-05 10:45:22 +0900

    add

--- inc/header.php
+++ inc/header.php
@@ -8,8 +8,9 @@ include_once $_SERVER['DOCUMENT_ROOT'].'/pinkping/inc/dbcon.php';
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>dashboard - pinkping</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+  
 </head>
+
 <body>
-  <h1>admin page</h1>
-</body>
-</html>
